d151a4f3e0eb5364c0241e99a72a83e3565c1a78
Vedant Dalvi <vedantdalvi20@gmail.com>
2017-11-21 14:34:25 +0530

    Update spinner.js

--- assets/spinner.js
+++ assets/spinner.js
@@ -11,15 +11,7 @@ $(function() {
 			$('.overlay').css({'display':'none'})
 		}, 1000);
 	});
-	// Will load page after 1min for users cannnot load properly
-	setTimeout(function() {
-		$('.overlay, .body').addClass('loaded');
-	}, 60000);
-	// Random text to keep it fresh dawg.
-	var names = ['Loading images...', 'Generating Colours...', 'Getting Fonts...', 'Testing connection...'];
-	setInterval(function() {
-	  var rand = Math.floor(Math.random() * 4);
-		$('#name').html(names[rand]);
-	}, 3500);
+	
+	
 	
 });
