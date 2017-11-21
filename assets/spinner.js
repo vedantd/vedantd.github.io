563e9d73ce19b2c9f56b85fd7c17a5b892ce74fb
Vedant Dalvi <vedantdalvi20@gmail.com>
2017-11-21 14:09:11 +0530

    Update spinner.js

--- assets/spinner.js
+++ assets/spinner.js
@@ -1,5 +1,25 @@
-var overlay=document.getElementById("overlay")
-
-window.addEventListener('load', function(){
-	overlay.style.display='none';
-})
+// JQUERY
+$(function() {
+	// Users can skip the loading process if they want.
+	$('.Skip').click(function() {
+		$('.overlay, .body').addClass('loaded');
+	})
+	// Will wait for everything on the page to load.
+	$(window).bind('load', function() {
+		$('.overlay, .body').addClass('loaded');
+		setTimeout(function() {
+			$('.overlay').css({'display':'none'})
+		}, 1000);
+	});
+	// Will load page after 1min for users cannnot load properly
+	setTimeout(function() {
+		$('.overlay, .body').addClass('loaded');
+	}, 60000);
+	// Random text to keep it fresh dawg.
+	var names = ['Loading images...', 'Generating Colours...', 'Getting Fonts...', 'Testing connection...'];
+	setInterval(function() {
+	  var rand = Math.floor(Math.random() * 4);
+		$('#name').html(names[rand]);
+	}, 3500);
+	
+});
